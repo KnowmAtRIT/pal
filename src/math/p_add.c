76a4bc9f980413a71f96db041e707296241968f1
Josh McSavaney <mcsaucy@csh.rit.edu>
2015-03-02 17:26:01 -0500

    Cleaned up basic p_math functions

--- src/math/p_add.c
+++ src/math/p_add.c
@@ -18,9 +18,8 @@
 
 void p_add_f32(float *a, float *b, float *c, int n)
 {
-
     int i;
     for (i = 0; i < n; i++) {
-        *(c + i) = *(a + i) + *(b + i);
+        *c++ = *a++ + *b++;
     }
 }
