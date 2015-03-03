f06ddb8f2d0dcc6ac252efd9493bb6266ccad086
Bryan T. Meyers <bmeyers@datadrake.com>
2015-03-03 12:04:36 -0500

    FIXED: function calls did not match new function defs

--- examples/base/simple_example.c
+++ examples/base/simple_example.c
@@ -7,10 +7,10 @@ int main(int argc, char *argv[])
     // Stack variables
     char *file = "./hello_task.elf";
     char *func = "main";
-    int status, i, all, nargs = 1;
+    int i, all, nargs = 1;
     char *args[nargs];
     char argbuf[20];
-
+    P_STATUS status;
     // References as opaque structures
     p_dev_t dev0;
     p_prog_t prog0;
@@ -18,10 +18,10 @@ int main(int argc, char *argv[])
     p_mem_t mem[4];
 
     // Execution setup
-    dev0 = p_init(DEMO, 0);        // initialize device and team
-    prog0 = p_load(dev0, file, func, 0); // load a program from file system
+    status = p_init(DEMO, 0, &dev0);        // initialize device and team
+    status = p_load(dev0, file, func, 0, &prog0); // load a program from file system
     all = p_query(dev0, NODES);   // find number of nodes in system
-    team0 = p_open(dev0, 0, all);        // create a team
+    status = p_open(dev0, 0, all, &team0);        // create a team
 
     // Running program
     for (i = 0; i < all; i++) {
